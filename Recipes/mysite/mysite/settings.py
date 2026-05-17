a0cbab0f659efb831b5e2c14c44dc135cf955e3e
Ramez Rizk <20240185@stud.fci-cu.edu.eg>
2026-05-17 19:51:50 +0300

    Update STATIC_URL and add STATIC_ROOT configuration

--- Recipes/mysite/mysite/settings.py
+++ Recipes/mysite/mysite/settings.py
@@ -118,11 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files (where the files are physically stored)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
